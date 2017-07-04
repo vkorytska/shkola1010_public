1fc7d1322d544271e0de56c66fd2720ab306d88b
boltik61 <boltik61@gmail.com>
2017-07-04 08:44:46 +0300

    Add forms

--- Lesson 7/src/app/app.component.ts
+++ Lesson 7/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
     selector: 'app-app',
     templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     pageTitle = 'Dev Book Store';
     searchBox = '';
+
+    private searchTermStream = new Subject<string>()
+
+    ngOnInit() {
+        this.searchTermStream
+            .debounceTime(1000)
+            .distinctUntilChanged()
+            .subscribe((term: string) => this.searchBox = term);
+    }
+
+    search(term: string) {
+        this.searchTermStream.next(term);
+    }
 }
