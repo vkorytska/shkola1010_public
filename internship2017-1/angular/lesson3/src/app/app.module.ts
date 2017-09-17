84d9cb437d3b0d13f5c65349480fdcf79ceacffa
boltik61 <boltik61@gmail.com>
2017-09-17 21:22:43 +0300

    Lesson 3

--- internship2017-1/angular/lesson3/src/app/app.module.ts
+++ internship2017-1/angular/lesson3/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BooksListComponent } from './books-list/books-list.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { UnlessDirective } from './directives/unless.directive';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		BooksListComponent,
-		FavoriteComponent
+		FavoriteComponent,
+		HighlightDirective,
+		UnlessDirective
 	],
 	imports: [
 		BrowserModule,
