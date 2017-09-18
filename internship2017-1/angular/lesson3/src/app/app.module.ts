1c773ca1a7c4fa7496d4a0e6e84e219738b7aeeb
boltik61 <boltik61@gmail.com>
2017-09-18 21:32:22 +0300

    Add pipe and fix tslint

--- internship2017-1/angular/lesson3/src/app/app.module.ts
+++ internship2017-1/angular/lesson3/src/app/app.module.ts
@@ -6,19 +6,21 @@ import { BooksListComponent } from './books-list/books-list.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { HighlightDirective } from './directives/highlight.directive';
 import { UnlessDirective } from './directives/unless.directive';
+import { BytesPipe } from './pipes/bytes.pipe';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		BooksListComponent,
-		FavoriteComponent,
-		HighlightDirective,
-		UnlessDirective
-	],
-	imports: [
-		BrowserModule,
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BooksListComponent,
+        FavoriteComponent,
+        HighlightDirective,
+        UnlessDirective,
+        BytesPipe
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
