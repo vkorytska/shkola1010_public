a1365d64cf0d45de3cdddd0e34cbabe541fc58cc
unknown <v.korytska@gmail.com>
2018-06-06 22:21:28 +0300

    HW1 - Add function confirmAge()

--- internship2018-1/src/index.js
+++ internship2018-1/src/index.js
@@ -1,3 +1,4 @@
 import {showMessage} from './user.service.js';
+import {confirmAge} from './user.service.js';
 
-showMessage();
+confirmAge();
--- internship2018-1/src/user.service.js
+++ internship2018-1/src/user.service.js
@@ -3,3 +3,13 @@ import { user } from './user.js';
 export function showMessage() {
     alert('Hello ' + user.name);
 }
+
+export function confirmAge() {
+    var input = window.prompt("What's your age?");
+
+    if ( input >= 18 ) {
+        showMessage();
+    }
+    location.href = "https://www.youtube.com/watch?v=OK03o3BHafk";
+    Location.assign(location.href);
+}
