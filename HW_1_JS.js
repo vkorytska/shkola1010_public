289c54008335229766bf886b754a1575c25a8f08
unknown <v.korytska@gmail.com>
2018-06-07 00:25:17 +0300

    HW_1_JS

--- HW_1_JS.js
+++ HW_1_JS.js
@@ -1,16 +1,44 @@
+//Task 1
 var numbers = [1,2,3,4,5,6,7,8,9,10],
-    min = 2,
-    max = 7;
+min = 2,
+max = 7;
 
 function filterArray(numbers, min, max) {
-    var newNumbers = [];
-    for (i=0; i < numbers.length; i++) {
-        if (numbers[i] >= min && numbers[i] <= max) {
-            newNumbers.push(numbers[i])
-        }
+var newNumbers = [];
+for (i=0; i < numbers.length; i++) {
+    if (numbers[i] >= min && numbers[i] <= max) {
+        newNumbers.push(numbers[i])
     }
-    return newNumbers
+}
+return newNumbers
 }
 
+filterArray(numbers, min, max);
 
 
+//Task 2
+var emptyObject = null,
+someObject = 1;
+
+function checkStatus(object){
+if(object == null){
+    return true;
+}
+return false;
+}
+
+checkStatus(emptyObject);
+checkStatus(someObject);
+
+//Task 3
+var person = {name: 'Vika', age:23};
+
+function getKey(object){
+var personArray = [];
+for (var key in object) {
+    personArray.push(key)
+}
+return personArray;
+}
+
+getKey(person);
